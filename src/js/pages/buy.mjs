d3235f7714a1576014a4ae907c85a6c9016b8161
kribac12 <89196018+kribac12@users.noreply.github.com>
2023-11-28 14:20:56 +0100

    fixed hash so user is taken to popular listings when clicking on the Buy-link

--- src/js/pages/buy.mjs
+++ src/js/pages/buy.mjs
@@ -1,8 +1,19 @@
 import '../userstate-display/logged-in-visible.mjs';
 import { setUpLogoutLink } from '../api/authenticate/logout.mjs';
 import { setUpTabs } from '../utilities/pills-categories.mjs';
+import { switchTab } from '../utilities/pills-categories.mjs';
 
 document.addEventListener('DOMContentLoaded', () => {
   setUpLogoutLink();
   setUpTabs();
+  handleFirstTabBasedOnHash();
 });
+
+function handleFirstTabBasedOnHash() {
+  const hash = window.location.hash;
+  if (hash === '#popular') {
+    switchTab('popularTab');
+  } else {
+    //
+  }
+}
