ec362e293f41a7a32f4944b8fd41f765ef3bbb45
kribac12 <89196018+kribac12@users.noreply.github.com>
2023-12-02 19:45:31 +0100

    small tweaks

--- src/js/api/bids/bids-service.mjs
+++ src/js/api/bids/bids-service.mjs
@@ -1,14 +1,13 @@
 import { makeApiRequest } from '../api-service.mjs';
 
 export async function placeBid(listingId, bidAmount) {
-  console.log(`Placing bid: ${bidAmount} on listing ${listingId}`);
   try {
     const response = await makeApiRequest(
       `listings/${listingId}/bids`,
       'POST',
       { amount: bidAmount }
     );
-    console.log('Bid placed successfully', response);
+
     return response;
   } catch (error) {
     console.error('Error placing bid:', error);
