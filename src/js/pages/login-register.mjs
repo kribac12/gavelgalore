2e551c13a49417fde016a965921b97c802ba6067
kribac12 <89196018+kribac12@users.noreply.github.com>
2023-12-14 14:10:17 +0100

    logout icon and sell-popup

--- src/js/pages/login-register.mjs
+++ src/js/pages/login-register.mjs
@@ -5,9 +5,17 @@ import '../userstate-display/logged-in-visible.mjs';
 import { setUpSearchForm } from '../setup/set-up-search.mjs';
 import { setupLoginFormValidation } from '../api/authenticate/login.mjs';
 import { setupRegisterFormValidation } from '../api/authenticate/register.mjs';
+import { handleActionForLoggedOutUsers } from '../utilities/modal-prompt.mjs';
 
 document.addEventListener('DOMContentLoaded', () => {
   setUpSearchForm();
   setupLoginFormValidation();
   setupRegisterFormValidation();
+
+  const sellLinks = document.querySelectorAll('.sell-action');
+  sellLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      handleActionForLoggedOutUsers('sell your items', event);
+    });
+  });
 });
